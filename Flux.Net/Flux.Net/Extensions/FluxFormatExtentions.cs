630d15ddff1461141159251bab940a4818b41cb0
Sylvain Bruyère <sylvain_bruyere@live.fr>
2023-12-01 20:23:24 +0100

    feat: add support for DateOnly (on .NET 6+) and handle null values in FluxFormatExtentions

--- Flux.Net/Flux.Net/Extensions/FluxFormatExtentions.cs
+++ Flux.Net/Flux.Net/Extensions/FluxFormatExtentions.cs
@@ -7,22 +7,34 @@ namespace Flux.Net.Extensions
 {
     public static class FluxFormatExtentions
     {
+        private const string NullValueMessage = "Cannot represent a null value in Flux.";
+
         public static string ToFlux(this object value) => value switch
         {
+            null => throw new ArgumentNullException(nameof(value), NullValueMessage),
             DateTime d => d.ToFlux(),
             DateTimeOffset d => d.ToFlux(),
+#if NET6_0_OR_GREATER
+            DateOnly d => d.ToFlux(),
+#endif
             Instant d => d.ToFlux(),
             OffsetDateTime d => d.ToFlux(),
             ZonedDateTime d => d.ToFlux(),
             LocalDateTime d => d.ToFlux(),
             bool b => b.ToFlux(),
             string s => s.ToFlux(),
-            _ => Convert.ToString(value, CultureInfo.InvariantCulture)
+            _ => Convert.ToString(value, CultureInfo.InvariantCulture)!
         };
 
         public static string ToFlux(this bool value) => value.ToString().ToLowerInvariant();
 
-        public static string ToFlux(this string value) => $"\"{value}\"";
+        public static string ToFlux(this string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), NullValueMessage);
+
+            return $"\"{value}\"";
+        }
 
         #region Date and time
 
@@ -37,6 +49,14 @@ namespace Flux.Net.Extensions
             // Format: "yyyy-MM-ddTHH:mm:ss.fffffffZ"
             return date.UtcDateTime.ToString("o");
         }
+
+#if NET6_0_OR_GREATER
+        public static string ToFlux(this DateOnly date)
+        {
+            // Format: "yyyy-MM-dd"
+            return date.ToString("o");
+        }
+#endif
 
         public static string ToFlux(this Instant date)
         {
